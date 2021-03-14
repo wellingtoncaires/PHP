04bf5c6630a0dbd6093c191f28224ce3b7644e49
Wellington Caires <well.caires@hotmail.com>
2021-03-13 22:15:07 -0300

    exercise php

---
+++ PHP7/tratamento_erro/try_catch.php
@@ -0,0 +1,23 @@
+<div class="titulo">Try/Catch</div>
+<?php
+// echo intdiv(7,0);
+
+try {
+    echo intdiv(7, 0);
+} catch(Error $e) {
+    echo 'Teve um erro aqui<br>';
+}
+
+try{
+    throw new Exception('Um erro muito estranho');
+    echo intdiv(7,0);
+} catch (DivisionByZeroError $e) {
+    echo 'Divisão por zero<br>';
+} catch(Throwable $e) {
+    echo 'Erro encontrado: ' . $e->getMessage() . '<br>';
+} finally {
+    echo 'Sempre executado!<br>';
+}
+
+
+echo 'Execução continua...<br>';
