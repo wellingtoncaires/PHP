4ae9aae7dab1dac8049d31516197fb1728b70e2e
Wellington Caires <well.caires@hotmail.com>
2021-03-23 07:22:53 -0300

    update

--- PHP7/api/download.php
+++ PHP7/api/download.php
@@ -5,32 +5,30 @@ session_start();
 
 $arquivos = $_SESSION['arquivos'] ?? [];
 
-$pastaUpload = __DIR__ . '/../files/';
+$pastaUpload = __DIR__ . 'files/';
 $nomeArquivo = $_FILES['arquivo']['name'];
-$arquivo = $pastaUpload . $nomeArquivo;
+$arquivo = $pastaUpload . $arquivo;
 $tmp = $_FILES['arquivo']['tmp_name'];
-
-if (move_uploaded_file($tmp, $arquivo)) {
-    echo "<br>Arquivo válido e enviado com sucesso.";
+if(move_uploaded_file($tmp, $arquivo)){
+    echo "<br> Arquivo válido e enviado com sucesso!";
     $arquivos[] = $nomeArquivo;
-    $_SESSION['arqivos'] = $arquivos;
+    $_SESSION['arquivos'] = $arquivos;
 } else {
-    echo "<br>Erro no upload doarquivo!";
-}
+    echo "<br> Erro no upload do arquivo!";
 ?>
 
-<form action="#" method="post" enctype="multipart/form-data">
+<form action="#" method="POST" enctype="multipart/form-data">
     <input name="arquivo" type="file">
     <button>Enviar</button>
 </form>
-
+    
 <ul>
     <?php foreach($arquivos as $arquivo): ?>
-        <li>
-            <a href="../files/<?= $arquivo ?>"download>
+       <li>
+            <a href="../files/<?= $arquivo ?>" download>
                 <?= $arquivo ?>
             </a>
-        </li>
+       </li>
     <?php endforeach ?>
 </ul>
 
